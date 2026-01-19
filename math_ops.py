efc0db5d20ee3a3037d22954807c8269e9569790
YalLi1 <pugachevaleksandr01@gmail.com>
2026-01-19 16:48:38 +0300

    Format parameters in add_numbers function

--- math_ops.py
+++ math_ops.py
@@ -2,7 +2,7 @@
 Простой модуль для математических операций.
 """
 
-def add_numbers(a: int | float, b: int | float) -> int | float:
+def add_numbers(  a: int | float, b  : int | float) -> int | float:
     """
     Возвращает сумму двух чисел.
 
