e05a53cb1e5043e05e88cf033f7230f4a7158cc0
YalLi1 <pugachevaleksandr01@gmail.com>
2026-01-19 16:40:34 +0300

    Добавить функции сложения и умножения
    Добавлен модуль для выполнения математических операций, включая функции сложения и умножения.

---
+++ math_ops.py
@@ -0,0 +1,30 @@
+"""
+Простой модуль для математических операций.
+"""
+
+def add_numbers(a: int | float, b: int | float) -> int | float:
+    """
+    Возвращает сумму двух чисел.
+
+    Args:
+        a: Первое число.
+        b: Второе число.
+
+    Returns:
+        Сумма a и b.
+    """
+    return a + b
+
+
+def multiply_numbers(a: int | float, b: int | float) -> int | float:
+    """
+    Возвращает произведение двух чисел.
+
+    Args:
+        a: Первое число.
+        b: Второе число.
+
+    Returns:
+        Произведение a и b.
+    """
+    return a * b
